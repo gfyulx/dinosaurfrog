5f081d8168a2aef29681dd113b679519974d41ab
gfyulx <gfyulx@gmail.com>
2018-08-17 17:27:05 +0800

    modify screenshot class

--- screenshot/screenshot.py
+++ screenshot/screenshot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # coding=utf-8
 
-import win32gui as win32
+import win32gui
 import win32api
 
 
@@ -24,7 +24,7 @@ class ScreenShot(object):
         self.currHandle = self.getForegroundWindow()
         self.screenSize = self.getScreenSize()
         self.checkFullScreen()
-
+        self.lcoor['lx'],self.lcoor['ly'],self.lcoor['rx'],self.lcoor['ry']=self.getWinRect(handle)
 
     def checkFullScreen(self):
         if (self.pcoor['rx']-self.pcoor['lx'] == self.screenSize['width']) \
@@ -38,35 +38,56 @@ class ScreenShot(object):
         high = win32api.GetSystemMetrics(1)
         return {'width': width, 'high': high}
 
+    #按类或者窗口名称返回窗口的主句柄
     def findWindByName(self, ClassName, WinName):
-        handle = win32.FindWindow(ClassName, WinName)
-        return handle
+        handle = win32gui.FindWindow(ClassName, WinName)
+        handleParent=win32gui.GetParent(handle)
+        if handleParent==0:
+            return handle
+        else:
+            return handleParent
     """
     根据句柄获取类名，方便下次查找
     """
 
     def getClassNameByHandle(self, pHandle):
-        className = win32.GetClassName(pHandle)
+        if pHandle>0:
+            className = win32gui.GetClassName(pHandle)
+        else:
+            className=None
         return className
 
     def getTitleByHandle(self, pHandle):
-        winText = win32.GetWindowText(pHandle)
+        winText = win32gui.GetWindowText(pHandle)
         return winText
     """
     获取当前活动窗口句柄
     """
 
     def getForegroundWindow(self):
-        currHandle = win32.GetForegroundWindow()
+        currHandle = win32gui.GetForegroundWindow()
         return currHandle
+    def getWinRect(self,handle):
+        """
+        left:窗口的左上坐标
+        top:窗口的顶部坐标
+        right:窗口的右上相对坐标
+        bottom:窗口的右下坐标
+        """
+        if handle>0:
+            return win32gui.GetWindowRect(handle)
+        else:
+            return 0,0,0,0
+        
+    #获取鼠标所在的坐标
+    def getMousePoint(self):
+        return win32gui.GetCursorPos()
+    #获取指定坐标点的窗口句柄
+    def getHandleByPoint(self,point):
+        try:
+            handle=win32gui.WindowFromPoint(point)
+        except Exception as e:
+            handle=0
+        return handle
 
 
-if __name__ == "__main__":
-    screenShotObj = ScreenShot()
-    handle = screenShotObj.findWindByName(None, "网易有道词典")
-    print("%0x" % handle)
-    print("%0x" % screenShotObj.getForegroundWindow())
-    print(screenShotObj.getTitleByHandle(handle))
-    print(screenShotObj.getClassNameByHandle(handle))
-    screenShotObj.getScreenSize()
-    screenShotObj.setAttr(handle)
