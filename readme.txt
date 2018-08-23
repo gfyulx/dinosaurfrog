c0eb9e5b22c4a1f072cc1a5ddf3596b8363a4940
gfyulx <gfyulx@gmail.com>
2018-08-23 11:16:55 +0800

    modify readme

--- readme.txt
+++ readme.txt
@@ -1,2 +1,2 @@
-This repo is used for Python code test
-#
+Record window content and analyze window content
+Use win32's API to intercept Windows and use opencv for image content analysis
--- screenshot/screenshot.py
+++ screenshot/screenshot.py
@@ -26,8 +26,8 @@ class ScreenShot(object):
         self.fullScreen = 0  # 是否全屏
 
     def WinCaptureToVideo(self,handle,secs,fileDir):       
-        fps=int(1000/secs)
-        #fps=25
+        #fps=int(1000/secs)
+        fps=10
         l,t,r,b=self.getWinRect(handle)
         fourcc=cv2.VideoWriter_fourcc('X','V','I','D')
         datenow=datetime.datetime.now()
@@ -49,24 +49,28 @@ class ScreenShot(object):
         saveBitMap.CreateCompatibleBitmap(mfcDC, w,h)
         saveDC.SelectObject(saveBitMap)  
         #saveDC.BitBlt((0,0),(w, h) , mfcDC, (l,t), win32con.SRCCOPY)   
-        
+        filePrx="%s/%d"%(fileDir,handle)
+        bmpname=filePrx+".bmp"   
         try:
             while True:
+                tB=time.time()
                 saveDC.BitBlt((0,0),(w, h) , mfcDC, (0,0), win32con.SRCCOPY)
-                datenow=datetime.datetime.now()
-                filePrx="%s/%d"%(fileDir,handle)
-                bmpname=filePrx+".bmp"       
+                #datenow=datetime.datetime.now()    
                 saveBitMap.SaveBitmapFile(saveDC, bmpname)
                 pic = Image.open(bmpname)
                 picNew=pic.crop((0,0,w,h))
-                os.remove(bmpname)
+                
                 #return picNew  #如果是视频模式，直接返回
                 #pic=self.WinCapture(handle,fileDir)
                 imm=cv2.cvtColor(np.array(picNew), cv2.COLOR_RGB2BGR)
                 video.write(imm)
+                os.remove(bmpname)
+                tE=time.time()
+                print(tE-tB)
                 #pic.close() 
                 #win32gui.DeleteObject(handle)
-                time.sleep(0.01)               
+                if (tE-tB<secs/1000):
+                    time.sleep(secs/1000-(tE-tB))              
         except Exception as e:
             print(e)
         finally:
@@ -184,6 +188,9 @@ class ScreenShot(object):
         
     #获取鼠标所在的坐标
     def getMousePoint(self):
+        hand=self.getForegroundWindow()
+        time.sleep(2)        
+        win32gui.ShowWindow(hand,2) #最小化当前窗口
         return win32gui.GetCursorPos()
     #获取指定坐标点的窗口句柄
     def getHandleByPoint(self,point):
@@ -192,7 +199,36 @@ class ScreenShot(object):
         except Exception as e:
             handle=0
         return handle
+    #获取全屏句柄
+    def getWinHandle(self):
+        return win32gui.GetDesktopWindow()
 
-
+if __name__ == "__main__":
+    screenShotObj = ScreenShot()
+    handle = screenShotObj.findWindByName(None,"优酷")
+    #handle = screenShotObj.findWindByName(None, "Chrome Legacy Window")
+    print("%0x" % handle)
+    print(handle)
+    #handle=0x10a1e
+    #print("%0x"%win32gui.GetParent(handle))
+    screenShotObj.setAttr(handle)
+    #screenShotObj.getWinRect(handle)
+    #print(handleNew)
+    #screenShotObj.setAttr(handleNew)
+    print(screenShotObj.className,screenShotObj.winText)
+    #print(screenShotObj.fullScreen)
+    #print("%0x"%win32gui.GetDC(handle))
+    #print(screenShotObj.lcoor['lx'],screenShotObj.lcoor['ly'],screenShotObj.lcoor['rx'],screenShotObj.lcoor['ry'])
+    #print(screenShotObj.findSubWinX(handle,1))
+    #每秒25帧。即40毫秒
+    #handle=0x317F8
+    #print("%d"%handle)
+    #screenShotObj.WinCaptureByTime(65552,40,"D:/code/python/resource")
+    #screenShotObj.getMousePoint()
+    screenShotObj.WinCaptureToVideo(65552,40,"D:/code/python/resource")
+    #screenShotObj.WinCapture(0xD10E2,"D:/code/python/resource",1)
+    #screenShotObj.WinCapture(0x10690,"D:/code/python/resource",1)
+    hDest=win32gui.GetDesktopWindow()
+    print(hDest)
    
 
