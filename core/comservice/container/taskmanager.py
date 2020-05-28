2e58c1989e103f0af0432bf33bb90a3df3a39b3c
gfyulx <yzonfu@linewell.com>
2020-05-28 16:25:11 +0800

    commit

--- core/comservice/container/taskmanager.py
+++ core/comservice/container/taskmanager.py
@@ -8,3 +8,8 @@
 Copyright:  Fujian Linewell Software Co., Ltd. All rights reserved.
 注意：本内容仅限于福建南威软件股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
 """
+
+
+class TaskManager():
+    def __init__(self):
+        pass
--- core/utils/diException.py
+++ core/utils/diException.py
@@ -1,16 +1,19 @@
 # -*- encoding: utf-8 -*-
 """
-@File    :   kubernetes.py    
+@File    :   diException.py    
 @Author  :   gfyulx@163.com
 @Version :    1.0
-@Description:容器接口服务
-@Modify TIme:  2020/5/28 14:37
+@Description:
+@Modify TIme:  2020/5/28 16:24
 Copyright:  Fujian Linewell Software Co., Ltd. All rights reserved.
 注意：本内容仅限于福建南威软件股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
 """
 
+class DiException(Exception):
 
-class KubernetesApi():
+    def __init__(self, message):
+        super().__init__(self)
+        self.message = message
 
-    def __init__(self):
-        pass
+    def __str__(self):
+        return self.message
